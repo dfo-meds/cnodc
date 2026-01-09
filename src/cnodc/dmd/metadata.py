a8ab480d729f735e2b8644855cde0870fa4848d9
Erin Turnbull <erin.a.turnbull@gmail.com>
2026-01-09 13:18:13 -0500

    Added variables to the metadata

--- src/cnodc/dmd/metadata.py
+++ src/cnodc/dmd/metadata.py
@@ -5,6 +5,238 @@ import datetime
 
 
 MultiLanguageString = t.Union[str, dict[str, str]]
+NumberLike = t.Union[int, str, float, decimal.Decimal]
+
+
+class Encoding(enum.Enum):
+
+    UTF8 = "utf8"  # strongly recommended
+    ISO_8859_1 = "iso-8859-1"
+    UTF16 = "utf16"
+
+
+class NetCDFDataType(enum.Enum):
+    String = "String"
+    Character = "char"
+    Double = "double"
+    Float = "float"
+    Long = "long"
+    LongUnsigned = "ulong"
+    Integer = "int"
+    IntegerUnsigned = "uint"
+    Short = "short"
+    ShortUnsigned = "ushort"
+    Byte = "byte"
+    ByteUnsigned = "ubyte"
+
+
+class Unit(enum.Enum):
+
+    # Latitude and Longitude
+    DegreesNorth = "degrees_north"
+    DegreesEast = "degrees_east"
+
+    # Angles (0 = North)
+    Degrees = "degree"
+
+    # Ratios
+    Ratio = "1"
+    Percent = "1e-2"
+    PartsPerThousand = "1e-3"
+    PartsPerMillion = "1e-6"
+    PartsPerBillion = "1e-9"
+
+    # Temperature
+    Celsius = "°C"
+    Kelvin = "K"
+
+    # Time
+    Day = "d"
+    Hour = "h"
+    Minute = "min"
+    Second = "s"
+
+    # Mass
+    Gram = "g"
+    Kilogram = "kg"
+    MetricTon = "t"
+
+    # Speed
+    Knot = "international_knot"
+    MetersPerSecond = "m s-1"
+    KilometersPerHour = "km h-1"
+
+    # Distance
+    Meter = "m"
+    NauticalMile = "nautical_mile"
+
+    # Voltage
+    Volt = "V"
+
+    # Pressure
+    Pascal = "Pa"
+    Hectopascal = "hPa"
+    Kilopascal = "kPa"
+    Atmosphere = "atm"
+    Bar = "bar"
+    Hectobar = "hbar"
+    Millibar = "mbar"
+
+    # Frequency
+    Hertz = "Hz"
+    PerSecond = "s-1"
+
+    # Mass Fraction
+    GramsPerKilogram = "g kg-1"
+
+    # Mass Flux
+    KilogramsPerSquareMeter = "kg m-2"
+
+    # Energy Flux
+    JoulesPerSquareMeter = "J m-2"
+
+    # Power Density
+    SquareMetersPerHertz = "m2 Hz-1"
+    SquareMeterSeconds = "m2 s"
+
+    # Power Density (by angle)
+    SquareMeterSecondsPerRadian = "m2 s rad-1"
+
+    # Molality
+    MicromolesPerKilogram = "umol kg-1"
+
+    # Molarity
+    MicromolesPerCubicMeter = "umol m-3"
+    MicromolesPerLitre = "umol L-1"
+    MillimolesPerCubicMeter = "mmol m-3"
+
+    # Density
+    KilogramsPerCubicMeter = "kg m-3"
+    MilligramsPerCubicMeter = "mg m-3"
+    MilligramsPerLitre = "mg L-1"
+
+    # Conductivity
+    SiemensPerMeter = "S m-1"
+
+
+class Calendar(enum.Enum):
+
+    Standard = "standard"
+    ProlepticGregorian = "proleptic_gregorian"
+    Julian = "julian"
+    Days365 = "noleap"
+    Days366 = "all_leap"
+    Days360 = "360_day"
+    Nonstandard = "nonstandard"
+
+
+class Direction(enum.Enum):
+
+    Up = "up"
+    Down = "down"
+
+
+class IOOSCategory(enum.Enum):
+
+    Acidity = "Acidity"
+    Bathymetry = "Bathymetry"
+    Biology = "Biology"
+    BottomCharacter = "Bottom Character"
+    CarbonDioxide = "CO2"
+    ColoredDissolvedOrganicMatter = "Color Dissolved Organic Matter"
+    Contaminants = "Contaminants"
+    Currents = "Currents"
+    DissolvedNutrients = "Dissolved Nutrients"
+    DissolvedOxygen = "Dissolved O2"
+    Ecology = "Ecology"
+    FishAbundance = "Fish Abundance"
+    FishSpecies = "Fish Species"
+    HeatFlux = "HeatFlux"
+    Hydrology = "Hydrology"
+    IceDistribution = "Ice Distribution"
+    Identifier = "Identifier"
+    Location = "Location"
+    Meteorology = "Meteorology"
+    OceanColor = "Ocean Color"
+    OpticalProperties = "Optical Properties"
+    Other = "Other"
+    Pathogens = "Pathogens"
+    PhytoplanktonSpecies = "PhytoplanktonSpecies"
+    Pressure = "Pressure"
+    Productivity = "Productivity"
+    Quality = "Quality"
+    Salinity = "Salinity"
+    SeaLevel = "Sea Level"
+    Statistics = "Statistics"
+    StreamFlow = "Stream Flow"
+    SurfaceWaves = "SurfaceWaves"
+    Taxonomy = "Taxonomy"
+    Temperature = "Temperature"
+    Time = "Time"
+    TotalSuspendedMatter = "Total Suspended Matter"
+    Unknown = "Unknown"
+    Wind = "Wind"
+    ZooplanktonSpecies = "Zooplankton Species"
+    ZooplanktonAbundance = "Zooplankton Abundance"
+
+
+class TimePrecision(enum.Enum):
+    Month = "month"
+    Day = "day"
+    Hour = "hour"
+    Minute = "minte"
+    Second = "second"
+    TenthSecond = "tenth_second"
+    HundredthSecond = "hundredth_second"
+    Millisecond = "millisecond"
+
+
+class NumericTimeUnits(enum.Enum):
+
+    Years = "years"
+    Months = "months"
+    Weeks = "weeks"
+    Days = "days"
+    Hours = "hours"
+    Minutes = "minutes"
+    Seconds = "seconds"
+    Milliseconds = "milliseconds"
+
+
+class TimeZone(enum.Enum):
+    UTC = "Etc/UTC"  # Strongly Recommended
+    CanadaEastern = "America/Toronto"
+    CanadaMountain = "America/Edmonton"
+    CanadaAtlantic = "America/Halifax"
+    CanadaCentral = "America/Winnipeg"
+    CanadaNewfoundland = "America/St_Johns"
+    CanadaPacific = "America/Vancouver"
+
+
+class ERDDAPVariableRole(enum.Enum):
+
+    ProfileExtra = "profile_extra"
+    TimeseriesExtra = "timeseries_extra"
+    TrajectoryExtra = "trajectory_extra"
+
+class CFVariableRole(enum.Enum):
+
+    ProfileID = "profile_id"
+    TimeseriesID = "timeseries_id"
+    TrajectoryID = "trajectory_id"
+
+
+class CoverageContentType(enum.Enum):
+
+    Auxillary = "auxillaryInformation"
+    Coordinate = "coordinate"
+    Image = "image"
+    ModelResult = "modelResult"
+    PhysicalMeasurement = "physicalMeasurement"
+    QualityInformation = "qualityInformation"
+    ReferenceInformation = "referenceInformation"
+    ThematicClassification = "thematicClassification"
+
 
 
 class GCAudience(enum.Enum):
@@ -257,18 +489,255 @@ class CoordinateReferenceSystem(enum.Enum):
     }
 
 
+class EntityRef:
+
+    def __init__(self):
+        self._guid = None
+        self._display_name = {}
+        self._metadata = {}
+
+    def set_guid(self, guid):
+        """
+        :param guid: A unique identifier for this entity
+        """
+        self._guid = guid
+
+    def set_display_name(self, display_name: MultiLanguageString):
+        """
+        :param display_name: A string or multi-language dict containing the internal display name of the entity.
+        """
+        if isinstance(display_name, str):
+            self._display_name = {
+                'und': display_name,
+            }
+        else:
+            self._display_name = display_name
+
+    def set_english_display_name(self, display_name: str):
+        """
+        :param display_name: The English display name
+        """
+        self._display_name['en'] = display_name
+
+    def set_french_display_name(self, display_name: str):
+        """
+        :param display_name: The French display name
+        """
+        self._display_name['fr'] = display_name
+
+    def build_request_body(self):
+        d = {
+            '_guid': self._guid,
+            '_display_name': self._display_name
+        }
+        d.update(self._metadata)
+        return d
+
+
+class Variable(EntityRef):
+
+    def __init__(self, var_name: str, var_data_type: NetCDFDataType):
+        super().__init__()
+        self.set_source_name(var_name)
+        self.set_source_data_type(var_data_type)
+
+    def set_encoding(self, enc: Encoding):
+        """
+        :param enc: The text encoding for this text field
+        """
+        self._metadata['encoding'] = enc.value
+
+    def set_source_name(self, name: str):
+        """
+        :param name: The name of this variable as it appears in the NetCDF file
+        """
+        self._metadata['source_name'] = name
+
+    def set_source_data_type(self, data_type: NetCDFDataType):
+        """
+        :param data_type: The type of the data as it is stored in a NetCDF file
+        """
+        self._metadata['source_data_type'] = data_type.value
+
+    def set_destination_data_type(self, data_type: NetCDFDataType):
+        """
+        :param data_type: The type of data once unpacked.
+        """
+        self._metadata['destination_data_type'] = data_type.value
+
+    def set_destination_name(self, name: str):
+        """
+        :param name: The name of the variable as it should appear in ERDDAP
+        """
+        self._metadata['destination_name'] = name
+
+    def set_dimensions(self, dims: list[str]):
+        """
+        :param dims: A list of dimensions that apply to this variable
+        """
+        self._metadata['dimensions'] = ",".join(dims)
+
+    def set_long_name(self, long_name: MultiLanguageString):
+        """
+        :param long_name: A string or multilanguage dict with the long name of the variable
+        """
+        self._metadata['long_name'] = {'und': long_name} if isinstance(long_name, str) else long_name
+
+    def set_standard_name(self, standard_name: StandardName):
+        """
+        :param standard_name: The standard name of the variable
+        """
+        self._metadata['standard_name'] = standard_name.value
+
+    def get_standard_name(self) -> t.Optional[str]:
+        if 'standard_name' in self._metadata:
+            return self._metadata['standard_name']
+        return None
+
+    def set_units(self, units: Unit):
+        """
+        :param units: The units for this variable
+        """
+        self._metadata['units'] = units.value
+
+    def set_time_precision(self,time_precision: TimePrecision):
+        """
+        :param time_precision: The precision of the time variable (e.g. is it every hour, every day, etc.)
+        """
+        self._metadata['time_precision'] = time_precision.value
+
+
+    def set_calendar(self, calendar: Calendar = Calendar.Standard):
+        """
+        :param calendar: The calendar used (the mixed Gregorian/Julian calendar typically used is the default)
+        """
+        self._metadata['calendar'] = calendar.value
+
+    def set_time_zone(self, time_zone: TimeZone = TimeZone.UTC):
+        """
+        :param time_zone: The time zone used (the default standard is UTC - caution: ERDDAP does not work well with numeric non-UTC times)
+        """
+        self._metadata['time_zone'] = time_zone.value
+
+    def set_numeric_time_units(self, base_units: NumericTimeUnits, epoch: datetime.datetime):
+        """
+        :param base_units: The basic units of duration (e.g. "seconds since")
+        :param epoch: The reference time
+        """
+        self._metadata['units'] = f"{base_units.value} since {epoch.isoformat("T")}"
+
+    def set_missing_value(self, missing_value: str):
+        """
+        :param missing_value: What should null values look like in the NetCDF file
+        """
+        self._metadata['missing_value'] = missing_value
+
+    def set_conversion(self, scale_factor: t.Optional[NumberLike] = "", add_offset: t.Optional[NumberLike] = ""):
+        """
+        :param scale_factor: The value to multiply the stored value by (default 1)
+        :param add_offset: The value to add to the stored value (default 0)
+        """
+        self._metadata['scale_factor'] = scale_factor
+        self._metadata['add_offset'] = add_offset
+
+    def set_ioos_category(self, category: IOOSCategory):
+        """
+        :param category: The IOOS category that best describes this variable
+        """
+        self._metadata['ioos_category'] = category.value
+
+    def set_actual_range(self, min_val: t.Optional[NumberLike] = None, max_val: t.Optional[NumberLike] = None):
+        """
+        :param min_val: The actual minimum value in the dataset
+        :param max_val: The actual maximum value in the dataset
+        """
+        self._metadata['actual_min'] = min_val
+        self._metadata['actual_max'] = max_val
+
+    def set_valid_range(self, min_val: t.Optional[NumberLike] = None, max_val: t.Optional[NumberLike] = None):
+        """
+        :param min_val: The minimum possible valid value in the dataset
+        :param max_val: The maximum possible valid value in the dataset
+        """
+        self._metadata['valid_min'] = min_val
+        self._metadata['valid_max'] = max_val
+
+    def set_allow_subsets(self, subsets: bool):
+        """
+        :param subsets: Whether to let users build subsets of data using this variable (recommended only for variables with a limited number of options)
+        """
+        self._metadata['allow_subsets'] = not not subsets
+
+    def set_role(self, role: t.Union[ERDDAPVariableRole, CFVariableRole]):
+        """
+        :param role: The role the variable plays in the dataset
+        """
+        if isinstance(role, ERDDAPVariableRole):
+            self._metadata['erddap_role'] = role.value
+        else:
+            self._metadata['cf_role'] = role.value
+
+    def set_comment(self, comment: str):
+        """
+        :param comment: Additional information on the variable
+        """
+        self._metadata['comment'] = comment
+
+    def set_references(self, references: str):
+        """
+        :param references: References for the variable
+        """
+        self._metadata['references'] = references
+
+    def set_source(self, source: str):
+        """
+        :param source: Where does the variable come from
+        """
+        self._metadata['source'] = source
+
+    def set_coverage_content_type(self, content_type: CoverageContentType):
+        """
+        :param content_type: Information about what the variable represents
+        """
+        self._metadata['coverage_content_type'] = content_type
+
+    def set_variable_order(self, order: int):
+        """
+        :param order: Higher numbers means the variable appears lower in the list of variables
+        """
+        self._metadata['variable_order'] = order
+
+    def set_is_axis(self, is_axis: bool):
+        """
+        :param is_axis: Set to true only in gridded datasets and only when the value is one of the axes
+        """
+        self._metadata['is_axis'] = not not is_axis
+
+    def set_is_altitude_proxy(self, is_proxy: bool):
+        """
+        :param is_proxy: Set to true if the variable is not altitude or depth but it can be a stand-in for them (like pressure)
+        """
+        self._metadata['altitude_proxy'] = not not is_proxy
+
 
 class DatasetMetadata:
 
     def __init__(self):
-        self._metadata = {}
-        self._act_workflow = None
-        self._pub_workflow = None
-        self._security_level = None
-        self._org_name = None
-        self._display_name = {}
-        self._users = set()
-        self._profiles = set('cnodc')
+        self._metadata: dict[str, t.Any] = {}
+        self._act_workflow: t.Optional[str] = None
+        self._pub_workflow: t.Optional[str] = None
+        self._security_level: t.Optional[str] = None
+        self._org_name: t.Optional[str] = None
+        self._display_name: dict[str, str] = {}
+        self._users: set[str] = set()
+        self._profiles: set[str] = set('cnodc')
+        self._variables: list[Variable] = []
+
+    def add_variable(self, var: Variable):
+        """
+        :param var: A variable to add to the dataset
+        """
+        self._variables.append(var)
 
     def add_profile(self, profile_name):
         """
@@ -609,7 +1078,7 @@ class DatasetMetadata:
 
 
     def build_request_body(self) -> dict:
-        return {
+        body = {
             'profiles': list(self._profiles),
             'org_name': self._org_name,
             'display_names': self._display_name,
@@ -619,4 +1088,9 @@ class DatasetMetadata:
             'publication_workflow': self._pub_workflow,
             'security_level': self._security_level,
         }
+        body['metadata']['variables'] = [
+            x.build_request_body()
+            for x in self._variables
+        ]
+        return body
 
