037a5caf0a0d32501f6d55c7a5ed5e60b76759a0
Erin Turnbull <erin.a.turnbull@gmail.com>
2026-02-03 15:35:28 -0500

    added users for the DMD file

--- src/cnodc/programs/glider/convert.py
+++ src/cnodc/programs/glider/convert.py
@@ -130,6 +130,9 @@ class OpenGliderConverter:
             main_link.set_resource_type(metadata.ResourceType.File)
             dist.set_primary_link(main_link)
             dmd.add_distribution_channel(dist)
+            if 'users' in self._mapping_data and self._mapping_data['users']:
+                for user in self._mapping_data['users']:
+                    dmd.add_user(user)
             dmd.set_erddap_info(
                 server=metadata.Common.ERDDAP_Primary,
                 dataset_id=mission_id,
