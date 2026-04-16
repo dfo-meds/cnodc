d1c982f9411e40dc45dd5de1a08cd8fad90fcded
Erin Turnbull <erin.a.turnbull@gmail.com>
2026-04-16 11:15:00 -0400

    renamed cache variable so it works properly with mixins

--- src/medsutil/cached.py
+++ src/medsutil/cached.py
@@ -4,18 +4,18 @@ class CachedObjectMixin:
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.__cache = {}
+        self._cache = {}
 
-    def _with_cache[X](self, key: t.Hashable, cb: t.Callable[..., X], *args, _clear_cache: bool = False, **kwargs) -> X:
-        if key not in self.__cache or _clear_cache:
-            self.__cache[key] = cb(*args, **kwargs)
-        return self.__cache[key]
+    def _with_cache[X](self, key: t.Hashable, cb: t.Callable[..., X], *args, _invalidate: bool = False, **kwargs) -> X:
+        if key not in self._cache or _invalidate:
+            self._cache[key] = cb(*args, **kwargs)
+        return self._cache[key]
 
     def _set_cache(self, key: t.Hashable, obj: t.Any):
-        self.__cache[key] = obj
+        self._cache[key] = obj
 
     def clear_cache(self, key: t.Optional[str] = None):
         if key is None:
-            self.__cache.clear()
-        elif key in self.__cache:
-            del self.__cache[key]
+            self._cache.clear()
+        elif key in self._cache:
+            del self._cache[key]
