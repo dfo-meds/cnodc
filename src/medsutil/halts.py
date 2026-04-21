9c04c711d08bf53e2646619c3883b708e4713372
Erin Turnbull <erin.a.turnbull@gmail.com>
2026-04-21 15:33:41 -0400

    improved halt flag's ability to read/write

--- src/medsutil/halts.py
+++ src/medsutil/halts.py
@@ -1,4 +1,5 @@
 import gzip
+import os
 import pathlib
 import shutil
 import typing as t
@@ -50,23 +51,37 @@ class HaltFlag:
             if not self.check_continue(raise_ex):
                 break
 
-    def read_all(self, readable: ct.SupportsBinaryRead, chunk_size: int = None):
-        chunk_size = chunk_size or DEFAULT_CHUNK_SIZE
-        while (data := readable.read(chunk_size)) not in (b'', ''):
-            yield data
-            self.check_continue()
+    def read_all(self, readable: ct.SupportsBinaryRead, chunk_size: int = None) -> t.Iterable[t.ByteString]:
+        if ct.is_binary_readable(readable):
+            chunk_size = chunk_size or DEFAULT_CHUNK_SIZE
+            while (data := readable.read(chunk_size)) not in (b'', ''):
+                yield data
+                self.breakpoint()
+        else:
+            raise TypeError(f'Type [{readable.__class__.__name__}] is not supported')
 
-    def write_all(self, writable: ct.SupportsBinaryWrite, data: t.Iterable):
-        for x in data:
-            writable.write(x)
-            self.check_continue()
+    def write_all(self, writable: ct.SupportsByteStreamWriting, data: t.Iterable, remove_on_halt: bool = True):
+        if ct.is_local_path(writable):
+            try:
+                with open(writable, 'wb') as h:
+                    self.write_all(h, data, remove_on_halt)
+            except HaltInterrupt:
+                if remove_on_halt and os.path.exists(writable):
+                    os.unlink(writable)
+                raise
+        elif ct.is_binary_writable(writable):
+            for x in data:
+                writable.write(x)
+                self.breakpoint()
+        elif isinstance(writable, bytearray):
+            for x in data:
+                writable.extend(x)
+                self.breakpoint()
+        else:
+            raise TypeError(f'Type [{writable.__class__.__name__}] is not supported')
 
-    def copy_data(self, readable: ct.SupportsBinaryRead, writable: ct.SupportsBinaryWrite, chunk_size: int = None):
-        chunk_size = chunk_size or DEFAULT_CHUNK_SIZE
-        while (data := readable.read(chunk_size)) not in (b'', ''):
-            self.check_continue()
-            writable.write(data)
-            self.check_continue()
+    def copy_data(self, readable: ct.SupportsByteStreaming, writable: ct.SupportsByteStreamWriting, chunk_size: int = None, remove_on_halt: bool = True):
+        self.write_all(writable, self.read_all(readable, chunk_size), remove_on_halt)
 
     @staticmethod
     def _iterate(iterable: t.Iterable, halt_flag=None, raise_ex: bool = True):
