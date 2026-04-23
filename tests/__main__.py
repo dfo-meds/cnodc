db4c87c04d53f4845630da0c6a4ceb3d5872289d
Erin Turnbull <erin.a.turnbull@gmail.com>
2026-04-23 16:19:38 -0400

    updated logging handlers for test cases

--- tests/__main__.py
+++ tests/__main__.py
@@ -7,10 +7,7 @@ import coverage
 TEST_DIR = pathlib.Path(__file__).absolute().parent
 
 if __name__ == '__main__':
-    logging.disable(logging.NOTSET)
-    logging.getLogger().setLevel('WARNING')
-    h = logging.StreamHandler(sys.stdout)
-    h.setFormatter(logging.Formatter('[%(levelname)s %(asctime)s]: %(message)s [%(name)s]', '%H:%M:%S'))
+
     new_argv = None
     if len(sys.argv) == 1:
         new_argv = [sys.argv[0], 'discover', '-s', 'tests', '-t', '.']
@@ -26,10 +23,14 @@ if __name__ == '__main__':
         config_file=TEST_DIR.parent / ".coveragerc",
         source_pkgs=["cnodc"]
     )
+
     with cov.collect():
+
         from pipeman.boot import init_cnodc
-        # speed up password hashing for tests only!
         init_cnodc('tests')
+        logging.disable(logging.NOTSET)
+
+        # speed up password hashing for tests only!
         import medsutil.secure as s
         s.DEFAULT_PASSWORD_HASH_ITERATIONS = 1
         s.MINIMUM_ITERATIONS = 2
