3024c528b1e1f8290cb3d10ac60d592bf39e3804
Erin Turnbull <erin.a.turnbull@gmail.com>
2026-05-22 08:10:23 -0400

    adapted glider workflow to work on source and filepayloads

--- src/pipeman/programs/glider/workers.py
+++ src/pipeman/programs/glider/workers.py
@@ -1,15 +1,14 @@
 import typing as t
-import uuid
 
 from autoinject import injector
 
-import nodb as nodb
+from medsutil.exceptions import CodedError
 from medsutil.ocproc2 import ParentRecord
 from pipeman.processing.queue_worker import QueueItemResult
-from pipeman.processing.payload_worker import SourceWorkflowWorker
+from pipeman.processing.payload_worker import PayloadWorker
 from pipeman.programs.nodb import NODBDecodeLoadWorker
 from medsutil.storage import StorageController, FilePath, StorageTier
-from pipeman.processing.payloads import SourceFilePayload
+from pipeman.processing.payloads import SourceFilePayload, Payload, FilePayload
 from pipeman.programs.glider.ego_convert import OpenGliderConverter
 
 
@@ -65,13 +64,12 @@ def add_glider_mission_platform_info(worker: NODBDecodeLoadWorker, record: Paren
                 memory['mission_map'][cruise_id] = mission.mission_uuid
                 record.metadata['CNODCMission'] = mission.mission_uuid
 
-
-class GliderConversionWorker(SourceWorkflowWorker):
+class GliderConversionWorker(PayloadWorker):
 
     storage: StorageController = None
 
     @injector.construct
-    def __init__(self, *args, **kwargs):
+    def __init__(self, **kwargs):
         super().__init__(
             process_name="glider_ego_converter",
             process_version="1.0",
@@ -101,10 +99,18 @@ class GliderConversionWorker(SourceWorkflowWorker):
         self._converter = OpenGliderConverter.build(halt_flag=self._halt_flag)
         super().on_start()
 
-    def process_payload(self, payload: SourceFilePayload) -> t.Optional[QueueItemResult]:
-        sf = payload.load_source_file(self.db)
-        self._log.info("Processing file %s", sf.source_path)
+    def process_payload(self, payload: Payload) -> t.Optional[QueueItemResult]:
+        if isinstance(payload, SourceFilePayload):
+            sf = payload.load_source_file(self.db)
+            filename = sf.file_name
+            filepath = sf.source_path
+        elif isinstance(payload, FilePayload):
+            filename = payload.filename
+            filepath = payload.file_path
+        else:
+            raise CodedError("Invalid payload type for worker", 1000, code_space="GLIDERCONVERT")
 
+        self._log.info("Processing file %s", filepath)
         local_file = self.download_to_temp_file()
 
         self._log.debug(f"Converting file and building metadata")
@@ -112,38 +118,38 @@ class GliderConversionWorker(SourceWorkflowWorker):
         file_name, mission_id, dmd_metadata = self._converter.convert(
             ego_file=local_file,
             og_file=new_file,
-            file_name=sf.file_name,
+            file_name=filename,
             autopublish=self.get_config('autopublish', True),
             gzip_erddap=self.get_config('gzip_erddap', True)
         )
 
         storage_locations = [
-            f'Original: {payload.load_source_file(self.db).source_path}'
+            f'Original: {filepath}'
         ]
         erddap_storage_metadata = self.storage.build_metadata(
             program_name='GLIDERS',
-            dataset_name=file_name[:-6]
+            dataset_name=mission_id
         )
         og_storage_metadata = erddap_storage_metadata.copy()
 
         gzip_file = self.temp_dir() / "openglider.nc.gz"
         gzipped = False
-        erddap_target_name = file_name
-        og_target_name = file_name
+        erddap_target_name = filename
+        og_target_name = filename
         og_file = new_file
         erddap_file = new_file
         if self.get_config('gzip_erddap', True):
             self._log.debug("Gzipped local file for upload")
             self.gzip_local_file(new_file, gzip_file)
             gzipped = True
-            erddap_target_name = file_name + '.gz'
+            erddap_target_name = filename + '.gz'
             erddap_storage_metadata['Gzip'] = 'YES'
             erddap_file = gzip_file
         if self.get_config('gzip_openglider', True):
             if not gzipped:
                 self._log.debug("Gzipped local file for upload")
                 self.gzip_local_file(new_file, gzip_file)
-            og_target_name = file_name + '.gz'
+            og_target_name = filename + '.gz'
             og_storage_metadata['Gzip'] = 'YES'
             og_file = gzip_file
 
