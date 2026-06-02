4aa80c9c3adb1411a3a24e59b9f06714e3512bf1
Erin Turnbull <erin.a.turnbull@gmail.com>
2026-06-02 17:06:45 -0400

    updated the require_permissions() function to be more efficient

--- src/medweb/apps/medsid/routes/user.py
+++ src/medweb/apps/medsid/routes/user.py
@@ -1,32 +1,44 @@
 from gcflask.i18n_url import MultiLanguageBlueprint
-
+from gcflask.security import require_permission
 user = MultiLanguageBlueprint('user', __name__, url_prefix="/medsid")
 
 
 @user.route('/me')
+@require_permission(authenticated_only=True)
 def me():
     ...
 
 @user.route('/me/edit', methods=['GET', 'POST'])
+@require_permission(authenticated_only=True)
 def edit():
     ...
 
 @user.route('/me/change-password', methods=['GET', 'POST'])
+@require_permission(authenticated_only=True)
 def change_password():
     ...
 
 @user.route('/me/permissions')
+@require_permission(authenticated_only=True)
 def permissions():
     ...
 
 @user.route('/users')
+@require_permission("medsid.user_management.view")
 def list_users():
     ...
 
-@user.route('/users/<username>', methods=['GET', 'POST'])
+@user.route('/users/<username>')
+@require_permission("medsid.user_management.view")
+def view_user(username):
+    ...
+
+@user.route('/users/<username>/edit', methods=['GET', 'POST'])
+@require_permission("medsid.user_management.edit")
 def edit_user(username):
     ...
 
 @user.route('/api/users')
-def users():
+@require_permission("medsid.user_management.view", is_api=True)
+def api_list_users():
     ...
