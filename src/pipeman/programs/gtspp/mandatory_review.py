ab133788a1373f27942c758632f7404e703c3b89
Erin Turnbull <erin.a.turnbull@gmail.com>
2026-06-11 17:16:26 -0400

    moved older tests

--- src/pipeman/programs/gtspp/mandatory_review.py
+++ src/pipeman/programs/gtspp/mandatory_review.py
@@ -1,3 +1,4 @@
+from medsutil.ocproc2 import QCResult
 from nodb.interface import NODB
 from nodb.observations import NODBPlatform
 from pipeman.programs.qc.base import DeepDiveChecker, ParentRecordRef, review
@@ -12,11 +13,10 @@ class GTSPPMandatoryManualReviewTest(DeepDiveChecker):
     def __init__(self):
         super().__init__('gtspp_mandatory_review', '1.0', test_tags=['GTSPP_5.1'])
 
-    @review("manual_review")
     def parent_record_check(self, ref: ParentRecordRef):
         with self.nodb as db:
             platform = ref.record.metadata.get('CNODCPlatform', None)
             self.require_value(platform)
             nodb_platform = NODBPlatform.find_by_uuid(db, platform.to_string())
             if nodb_platform is not None and nodb_platform.metadata.get("require_review", False):
-                self.report_qc_error("manual_review_required")
+                self.update_qc_result(QCResult.MANUAL_REVIEW)
