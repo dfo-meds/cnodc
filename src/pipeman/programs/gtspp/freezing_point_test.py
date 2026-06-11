a709d3504d4ce2eb5c62d476acf934d8b4d024c7
Erin Turnbull <erin.a.turnbull@gmail.com>
2026-06-11 17:16:26 -0400

    moved older tests

--- src/pipeman/programs/gtspp/freezing_point_test.py
+++ src/pipeman/programs/gtspp/freezing_point_test.py
@@ -14,21 +14,21 @@ class GTSPPFreezingPointTest(DeepDiveChecker):
 
     TRACK_COORDINATES = True
 
-    def __init__(self, **kwargs):
+    def __init__(self, aggressive_mode: bool = False):
         super().__init__('gtspp_freezing', '1.0', test_tags=['GTSPP_2.6'])
+        self._aggressive_mode = aggressive_mode
 
     def record_check(self, ref: RecordRef):
-        freezing_points = [x for x in self.get_freezing_points(ref)]
-        # need at least one salinity to check against
-        if not freezing_points:
-            return
         temp_ref = self.get_record_parameter_ref(ref, "Temperature")
         if temp_ref is not None:
-            for temp_sref in self.iterate_on_single_elements(temp_ref):
-                self.freezing_point_test(
-                    temp_sref,
-                    freezing_points
-                )
+            freezing_points = [x for x in self.get_freezing_points(ref)]
+            if freezing_points:
+                freezing_point = max(freezing_points) if self._aggressive_mode else min(freezing_points)
+                for temp_sref in self.extract_good_values(temp_ref):
+                    self.freezing_point_test(
+                        temp_sref,
+                        freezing_point
+                    )
 
     def get_freezing_points(self, ref: RecordRef) -> t.Iterable[amath.AnyNumber]:
         # in degrees_C
@@ -47,21 +47,11 @@ class GTSPPFreezingPointTest(DeepDiveChecker):
                 yield fp
 
     @review("above_freezing", fail_flag=3, skip_dubious=True, pass_flag=1)
-    def freezing_point_test(self, ref: SingleElementRef, freezing_points: list[amath.AnyNumber]):
+    def freezing_point_test(self, ref: SingleElementRef, freezing_point: amath.AnyNumber):
         temp = omath.get_temperature(
             temperature=ref.element,
-            temperature_scale=TemperatureScale.TS_1990,
+            obs_date=self.current_time,
             units="degrees_C",
-            obs_date=self.current_time
+            temperature_scale=TemperatureScale.TS_1990
         )
-        for fp in freezing_points:
-            if amath.
-        self.assert_greater_or_close(temp,)
-
-    def _test_freezing_point(self, v: ocproc2.AbstractElement, ctx: TestContext, fp: float):
-        self.should_test_value(v)
-
-        if temp > 0:
-            return
-        self.assert_greater_than('fp_temp_too_low', temp, fp, qc_flag=13)
-
+        self.assert_greater_or_close(t.cast(amath.AnyNumber, temp), freezing_point)
