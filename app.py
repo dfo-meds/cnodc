fba80bae4336aa6086e957e63913022d720d23f8
Erin Turnbull <erin.a.turnbull@gmail.com>
2026-07-22 16:26:10 -0400

    changed app.py to run the medweb app

--- app.py
+++ app.py
@@ -3,5 +3,6 @@ import sys
 ROOT_DIR = pathlib.Path(__file__).parent.absolute().resolve()
 sys.path.append(str(ROOT_DIR / "src"))
 
-from pipeman_web.boot import build_cnodc_webapp
-app = build_cnodc_webapp(__name__, sys.argv[0].replace("\\", "/").endswith("flask/__main__.py"))
+from medweb.app import create_app
+app = create_app()
+
