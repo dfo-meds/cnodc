9410bf91d61719bbc2fc848f255a06ad10458336
Erin Turnbull <erin.a.turnbull@gmail.com>
2026-07-24 11:54:19 -0400

    moved savedata over to its own class so I can save preferences with it too

--- src/pipeman/processing/base_worker.py
+++ src/pipeman/processing/base_worker.py
@@ -1,19 +1,17 @@
-import json
-import os
 import pathlib
 import tempfile
 import time
 import typing as t
 
-import zrlog
 from autoinject import injector
 
 import nodb.interface as interface
 from medsutil.cached import CachedObjectMixin, cached_method
 from medsutil.dynamic import dynamic_object, DynamicObjectLoadError
-from medsutil.exceptions import CodedError, HaltInterrupt
+from medsutil.exceptions import HaltInterrupt
 from medsutil.halts import HaltFlag, gzip_with_halt, ungzip_with_halt
 from medsutil.metrics import Gauge, Histogram
+from medsutil.savedata import SaveData
 
 from medsutil.storage import StorageController, FilePath
 from pipeman.instrumented import InstrumentedObject
@@ -303,56 +301,3 @@ class BaseWorker(CachedObjectMixin, InstrumentedObject):
         ungzip_with_halt(source_file, destination_file, halt_flag=self._halt_flag)   # pragma: no coverage
 
 
-class SaveData:
-    """Represents data that should be saved to the hard drive while running a process.
-        Note that save data may be wiped if running on a virtual machine, so process workers
-        cannot rely on the data being there and should have sensible default behaviour.
-
-        Save data must be compatible with JSON objects.
-    """
-
-    def __init__(self, save_file: t.Union[str, pathlib.Path, None]):
-        self._save_file = pathlib.Path(save_file) if isinstance(save_file, str) else save_file
-        self._file_loaded: bool = False
-        self._save_failed: bool = False
-        self._values = {}
-
-    def __getitem__(self, item):
-        self.load_file()
-        return self._values[item]
-
-    def __setitem__(self, key, value):
-        self.load_file()
-        self._values[key] = value
-
-    def __contains__(self, key):
-        self.load_file()
-        return key in self._values
-
-    def get(self, item, default=None):
-        self.load_file()
-        if item in self._values:
-            return self._values[item]
-        return default
-
-    def load_file(self):
-        """Ensure that the file is loaded, if it exists."""
-        if not self._file_loaded:
-            self._file_loaded = True
-            if self._save_file and self._save_file.exists():
-                try:
-                    with open(self._save_file, "r") as h:
-                        self._values = json.loads(h.read()) or {}
-                except OSError as ex:
-                    zrlog.get_logger("cnodc.save_file").exception("Exception while opening save file")
-
-    def save_file(self):
-        """Save the data to disk."""
-        if self._save_file is not None and self._file_loaded and not self._save_failed:
-            try:
-                self._save_file.parent.mkdir(mode=0o660, parents=True, exist_ok=True)
-                with open(self._save_file, "w") as h:
-                    h.write(json.dumps(self._values))
-            except OSError as ex:
-                zrlog.get_logger("cnodc.save_file").exception("Exception while saving save data")
-                self._save_failed = True
