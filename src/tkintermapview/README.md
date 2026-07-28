ed8100b5a8895683e135276d31b081a927fee7c6
Erin Turnbull <erin.a.turnbull@gmail.com>
2026-07-28 15:13:27 -0400

    fork of tkintermapview

---
+++ src/tkintermapview/README.md
@@ -0,0 +1,3 @@
+This is a fork of https://github.com/TomSchimansky/TkinterMapView/
+
+I'm fixing some decent issues with it as I find them. One day I'd like to work on contributing them back to the repo.
---
+++ src/tkintermapview/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "1.30"
+
+from .map_widget import TkinterMapView
+from .offline_loading import OfflineLoader
+from .utility_functions import convert_coordinates_to_address, convert_coordinates_to_country, convert_coordinates_to_city
+from .utility_functions import decimal_to_osm, osm_to_decimal
