f041b7505b2f2543922a0736f3b0c74f2b102903
Erin Turnbull <erin.a.turnbull@gmail.com>
2026-09-11 10:57:55 -0400

    added base workflow stubs

---
+++ src/dmd/plugins/workflow_base/util.py
@@ -0,0 +1,40 @@
+
+
+def step_publish_dataset():
+    ...
+
+
+def step_activate_dataset():
+    ...
+
+
+def step_block_dataset_edits():
+    ...
+
+
+def step_return_to_draft():
+    ...
+
+
+def step_remove_translation_flag():
+    ...
+
+
+def step_fetch_translation():
+    ...
+
+
+def step_noop():
+    ...
+
+
+def step_start_publication():
+    ...
+
+
+def step_send_dataset_email():
+    ...
+
+
+def step_upload_metadata_from_template():
+    ...
